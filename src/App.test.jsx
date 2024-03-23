75a60b929259e53f58001bcd7948ef65d8ae2d35
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-03-23 13:40:23 +0100

    Change styling and remove unnecessary code

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,14 +2,24 @@ import { screen, render } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
 import App from "./App";
 import Cart from "./Cart";
+import Product from "./Product";
 import { BrowserRouter } from "react-router-dom";
 describe("App", () => {
   it("Renders correctly", () => {
     const { container } = render(
       <BrowserRouter>
-        <App />
+        <App></App>
       </BrowserRouter>
     );
+    const { Product } = render(<Product />);
     expect(container).toMatchSnapshot();
+  });
+  it("Adds to cart", () => {
+    const { container } = render(
+      <BrowserRouter>
+        <App></App>
+      </BrowserRouter>
+    );
+    const { Product } = render(<Product />);
   });
 });
